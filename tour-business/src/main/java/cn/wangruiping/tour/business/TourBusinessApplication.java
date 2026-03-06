05e4011925b81499bc8734e0c14b5d5e8e9df6e0
wrp <1345646773@qq.com>
2026-03-06 16:49:04 +0800

    项目创建

---
+++ tour-business/src/main/java/cn/wangruiping/tour/business/TourBusinessApplication.java
@@ -0,0 +1,13 @@
+package cn.wangruiping.tour.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourBusinessApplication.class, args);
+    }
+
+}
